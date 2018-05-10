2999dd0b7f5fa8a3df50fd5731508e9cee77d6aa
David Moulton <d.moulton@elifesciences.org>
2018-05-10 16:54:20 +0100

    Add js build to pipeline

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,13 @@
 'use strict';
 
 const autoprefixer = require('gulp-autoprefixer');
+const babel = require('babelify');
+const browserify = require('browserify');
 const browserSync = require('browser-sync').create();
+const buffer = require('vinyl-buffer');
 const del = require('del');
+const eslint = require('gulp-eslint');
+const gutil = require('gulp-util');
 const gulp = require('gulp');
 const minimist = require('minimist');
 const postcss = require('gulp-postcss');
@@ -11,6 +16,7 @@ const reporter = require('postcss-reporter');
 const sourcemaps = require('gulp-sourcemaps');
 const sass = require('gulp-sass');
 const sassGlob = require('gulp-sass-glob');
+const source = require('vinyl-source-stream');
 const stylelint = require('stylelint');
 const syntaxScss = require('postcss-scss');
 
@@ -32,7 +38,8 @@ const config = {
 };
 
 config.sources.files = {
-  scss: `${config.sources.directories.scss}/**/*.scss`
+  scss: `${config.sources.directories.scss}/**/*.scss`,
+  js: `${config.sources.directories.js}/**/*.js`
 };
 
 gulp.task('generateAllStyles', ['sass:lint'], () => {
@@ -62,5 +69,33 @@ gulp.task('sass:clean', () => {
   del(config.outputPaths.css);
 });
 
-gulp.task('build', ['generateAllStyles']);
+gulp.task('js', ['js:transpile']);
+
+gulp.task('js:transpile', ['js:clean', 'js:lint'], () => {
+  return browserify(`./${config.sources.directories.js}/main.js`, {
+    debug: true /* !(environment === 'production') */
+  })
+    .transform(babel)
+    .bundle()
+    .on('error', (err) => { console.error(err.message); })
+    .pipe(source('main.js'))
+    .pipe(buffer())
+    .pipe(sourcemaps.init({ loadMaps: true }))
+    .pipe(environment === 'production' ? uglify() : gutil.noop())
+    .pipe(sourcemaps.write('./'))
+    .pipe(gulp.dest(config.outputPaths.js));
+});
+
+gulp.task('js:clean', () => {
+  del(config.outputPaths.js);
+});
+
+gulp.task('js:lint', () => {
+  return gulp.src([config.sources.files.js])
+             .pipe(eslint())
+             .pipe(eslint.format())
+             .pipe(eslint.failAfterError());
+});
+
+gulp.task('build', ['generateAllStyles', 'js']);
 gulp.task('default', ['build']);
---
+++ src/js/main.js
@@ -0,0 +1,3 @@
+'use strict';
+
+window.console.log('Hello world');
