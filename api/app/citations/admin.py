8b7025d20a82dc20433a3086b4274801789602b0
Sean Wiseman <seanwiseman2012@gmail.com>
2018-05-11 10:23:40 +0100

    Add custom admin model for citation

--- api/app/citations/admin.py
+++ api/app/citations/admin.py
@@ -3,4 +3,21 @@ from django.contrib import admin
 from citations.models import Citation
 
 
-admin.site.register(Citation)
+class CitationAdmin(admin.ModelAdmin):
+    list_per_page = 20
+
+    list_filter = (
+        'id',
+    )
+
+    list_display = (
+        'identifier',
+        'type',
+        'id',
+        'timestamp',
+    )
+
+    list_display_links = ['identifier']
+
+
+admin.site.register(Citation, CitationAdmin)
