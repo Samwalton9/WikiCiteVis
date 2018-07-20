567888ff1f07be6f98d40ebdaab3b612977d4e5e
Sean Wiseman <seanwiseman2012@gmail.com>
2018-07-20 11:21:09 +0100

    Add datetime filter

--- api/app/citations/api.py
+++ api/app/citations/api.py
@@ -19,12 +19,21 @@ class CitationViewSet(viewsets.ModelViewSet):
         'id',
     )
 
+    @staticmethod
+    def filter_timestamp_range(qs, start_date, end_date):
+        return qs.filter(timestamp__range=[start_date, end_date])
+
     def get_queryset(self):
         order_by = self.request.query_params.get('orderBy', None)
+        start_date = self.request.query_params.get('startDate', None)
+        end_date = self.request.query_params.get('endDate', None)
 
         if order_by:
             queryset = self.queryset.order_by(order_by)
         else:
             queryset = self.queryset.all()
+
+        if start_date and end_date:
+            queryset = self.filter_timestamp_range(queryset, start_date, end_date)
 
         return queryset
